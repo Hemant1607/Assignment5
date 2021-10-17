412e38781ceed32262d4ccf378196880dbdbf532
Hemant1607 <hemant1607.hk@gmail.com>
2021-10-18 01:36:55 +0530

    Assignment 5:EventManagementApp

---
+++ EventManagementApp/EventManagementApp/BookingDetails.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using System.Data;
+using EventDataClass;
+using EventBusiness;
+
+
+namespace EventManagementApp
+{
+    public partial class BookingDetails : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            gvBookingDetails.DataBind();
+        }
+    }
+}
